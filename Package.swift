0a30123f1514827da04deece098b540c88da61a5
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-08-31 10:08:33 -0500

    add dependency to binary target

--- Package.swift
+++ Package.swift
@@ -1,46 +1,29 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let packageVersion = "0.0.1"
+
 let package = Package(
-    name: "TruVideoCore",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v16)        
-    ],
+    name: "TruvideoSdk",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TruVideoCore",
-            targets: ["TruVideoCore"]),
+            name: "TruvideoSdk",
+            targets: ["TruvideoSdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/Truvideo/truvideo-sdk-ios-common.git", from: "0.0.2"),
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TruVideoCore",
-            dependencies: [
-                "Alamofire",
-                .product(name: "Common", package: "truvideo-sdk-ios-common"),
-                "KeychainAccess"
-            ],
-            path: "Sources"
+        .binaryTarget(
+            name: "TruvideoSdk",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(packageVersion)/TruvideoSdk.xcframework.zip",
+            checksum: "d653fb383198d40d1c7703069208b1ea371ea9840987523a22d296fd70f1812e"
         ),
-        .testTarget(
-            name: "TruVideoCoreTests",
-            dependencies: ["TruVideoCore"],
-            path: "Tests",
-            resources: [
-                .process("Test Resources")
-            ]
-        ),
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
