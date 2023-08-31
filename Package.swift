a1325e928521df08cae88b54e72f31159d01b29f
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-08-31 15:55:37 -0500

    chore: 0.0.4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@
 
 import PackageDescription
 
-let packageVersion = "0.0.3"
+let commonPackageVersion = "0.0.3"
+let packageVersion = "0.0.4"
 
 let package = Package(
     name: "TruvideoSdk",
@@ -11,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TruvideoSdk",
-            targets: ["TruvideoSdk"]),
+            targets: ["TruvideoSdkTargets"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,9 +22,22 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
+            name: "Common",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(commonPackageVersion)/Common.xcframework.zip",
+            checksum: "52c0e808ce21419edb3f0b6f2c9551821f8995caa975db0b901c6afad176610f"
+        ),
+        .binaryTarget(
             name: "TruvideoSdk",
             url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/\(packageVersion)/TruvideoSdk.xcframework.zip",
             checksum: "13ba616fc8fb88b04b8d5770ca6e6ce54d08d0f7d3899c5379fb57e156ab4fab"
         ),
+        .target(
+            name: "TruvideoSdkTargets",
+            dependencies: [
+                .target(name: "Common"),
+                .target(name: "TruvideoSdk")
+            ],
+            path: "Sources"
+        )
     ]
 )
