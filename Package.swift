6fd7a825b3e8fcf430c6a4f41b2b7e26a5a31b6e
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-09-13 09:44:20 -0500

    chore: bump to 0.0.6

--- Package.swift
+++ Package.swift
@@ -3,8 +3,9 @@
 
 import PackageDescription
 
-let commonPackageVersion = "0.0.4"
-let packageVersion = "0.0.5"
+let commonPackageVersion = "0.0.1"
+let packageVersion = "0.0.6"
+let sharedPackageVersion = "0.0.3"
 
 let package = Package(
     name: "TruvideoSdk",
@@ -23,18 +24,24 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Common",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(commonPackageVersion)/Common.xcframework.zip",
-            checksum: "4e029bf878f4d9a6eecd9a9ac695de840b204f0bba0181681fc583161e69a362"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common-wrapper/releases/download/\(commonPackageVersion)/Common.xcframework.zip",
+            checksum: "0d4b5f63a41b265a9522f8957785bdb04c355a325cb38a9bff760281e96d7d38"
+        ),
+        .binaryTarget(
+            name: "Shared",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(sharedPackageVersion)/shared.xcframework.zip",
+            checksum: "5028e25b25b24e6f35fbe4484dfc438a31345a9d5bf5666640e99849cfb497cb"
         ),
         .binaryTarget(
             name: "TruvideoSdk",
             url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/\(packageVersion)/TruvideoSdk.xcframework.zip",
-            checksum: "13ba616fc8fb88b04b8d5770ca6e6ce54d08d0f7d3899c5379fb57e156ab4fab"
+            checksum: "80fe73db1d5fb2f365f787cb6176937dd54d5afa3d7eba7ba7536f17a0a8e1c1"
         ),
         .target(
             name: "TruvideoSdkTargets",
             dependencies: [
                 .target(name: "Common"),
+                .target(name: "Shared"),
                 .target(name: "TruvideoSdk")
             ],
             path: "Sources"
