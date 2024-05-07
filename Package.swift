71d07626615370a59ec96c13efb988f610971f7e
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-05-07 19:29:12 +0000

    Update 68.1.1-beta.100

--- Package.swift
+++ Package.swift
@@ -15,19 +15,19 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdk",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/67.1.1-beta.99/TruvideoSdk.xcframework.zip",
-            checksum: "18ac4eb1b515aaa35718b5be5463c008d73047387696dd9b4fa7a54807f08a7f"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/68.1.1-beta.100/TruvideoSdk.xcframework.zip",
+            checksum: "4da310b7144bd4b726bde69172b462c3c509f78254c2fcca5cfcfa1a681da6b2"
         ),
         .binaryTarget(
-            name: "Shared",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-shared/releases/download/0.0.67/shared.xcframework.zip",
-            checksum: "c39cb1fb40f568b7329e46ab357c7f224addda6f24003b3f100d8e1854263040"
+            name: "Common",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/0.0.68/shared.xcframework.zip",
+            checksum: "563252e2b56a66dc0b5d9d313fa33a361a83a5b5db98ce740d47eeb0023ddc09"
         ),
         .target(
             name: "TruvideoSdkTargets",
             dependencies: [
                 .target(name: "TruvideoSdk"),
-                .target(name: "Shared"),
+                .target(name: "Common"),
                 .product(name: "AWSS3", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
