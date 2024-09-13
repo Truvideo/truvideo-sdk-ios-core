3bb677d831331ff3a8bfb358ef7cbc54180f3e06
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-09-13 18:36:11 +0000

    Update 75.1.1-RC.135

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdk",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/73.1.2-BETA.121/TruvideoSdk.xcframework.zip",
-            checksum: "7e8c643e05cc02dd735731f150e14319b26f1c2210286f8761ba6da761849132"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/75.1.1-RC.135/TruvideoSdk.xcframework.zip",
+            checksum: "ebf4af09262b919064f857a7c8ac490e08ab9ecf9424dcc4eb64d95f98bf7805"
         ),
         .binaryTarget(
             name: "Common",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/0.0.73/shared.xcframework.zip",
-            checksum: "967527b6f0591c10453561b6ca2f9ac29acf729d6d739985cd2508e2a0adb378"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/0.0.75/shared.xcframework.zip",
+            checksum: "7aee69571862f3e552832eaa12914da693563bca84f537eb83173f8b64958457"
         ),
         .target(
             name: "TruvideoSdkTargets",
