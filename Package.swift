e9f138d143242882145c80080ce5fc54013625e1
Bitrise CI <noreply@bitrise.io>
2025-06-09 20:02:28 +0000

    Update 76.2.1-DEV.219

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdk",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/76.2.1/TruvideoSdk.xcframework.zip",
-            checksum: "9a8cf47dd153f322e2ade27de404cb4cdbc9ba1e3274ed3d0dac8fc435db534f"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/76.2.1-DEV.219/TruvideoSdk.xcframework.zip",
+            checksum: "bf1efdeeb9fb7b95459248008861dc8ed2a33bba47aeae692152673f33c215e2"
         ),
         .binaryTarget(
             name: "Common",
